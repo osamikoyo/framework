684d662b3c429a6cfcd9c96dff7038ec47b4ca99
osami <v.pylayev@mail.ru>
2024-04-10 13:08:48 +0400

    df

--- mongodb.go
+++ mongodb.go
@@ -1,14 +1,59 @@
 package main
 
 import (
-
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-
+"context"
+"fmt"
+"go.mongodb.org/mongo-driver/mongo"
+"go.mongodb.org/mongo-driver/mongo/options"
 )
-
 type Mongo interface{
 	add(collect string)
     del(filter string)
 	scan(filter string)
 }
+
+type Animal struct{
+  age int
+  name string
+}
+func (a Animal) add(url string){
+   // Set client options
+clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+
+// Connect to MongoDB
+client, err := mongo.Connect(context.TODO(), clientOptions)
+if err != nil {
+	fmt.Println("Error connecting to MongoDB:", err)
+	return
+}
+
+// Check the connection
+err = client.Ping(context.TODO(), nil)
+if err != nil {
+	fmt.Println("Error pinging MongoDB:", err)
+	return
+}
+
+// Access the database
+database := client.Database("myDatabase")
+
+// Access the collection
+collection := database.Collection("myCollection")
+
+// Define a document
+document := bson.M{
+	"key": "value",
+}
+
+// Insert the document
+result, err := collection.InsertOne(context.TODO(), document)
+if err != nil {
+	fmt.Println("Error inserting document:", err)
+	return
+}
+
+// Print the ID of the inserted document
+fmt.Println("Inserted document ID:", result.InsertedID)
+}
+

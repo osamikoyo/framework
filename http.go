95941f59c2e7a08c4b095326e19ed84eae37565d
osami <v.pylayev@mail.ru>
2024-04-12 20:58:43 +0400

    i love peach

--- http.go
+++ http.go
@@ -10,24 +10,4 @@ import(
 	   tmpl.Execute(w, tmpl)
 
     })
-    http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request){
-		tmpl, _ := template.ParseFiles("./tmpl/sd.html")
-		tmpl.Execute(w, tmpl)
- 
-	 })
-	 http.HandleFunc("/b", func(w http.ResponseWriter, r *http.Request){
-		tmpl, _ := template.ParseFiles("./tmpl/ls.html")
-		tmpl.Execute(w, tmpl)
- 
-	 })
-	 http.HandleFunc("/c", func(w http.ResponseWriter, r *http.Request){
-		tmpl, _ := template.ParseFiles("./tmpl/os.html")
-		tmpl.Execute(w, tmpl)
- 
-	 })
-	 http.HandleFunc("/d", func(w http.ResponseWriter, r *http.Request){
-		tmpl, _ := template.ParseFiles("./tmpl/main.html")
-		tmpl.Execute(w, tmpl)
- 
-	 })
  }
